a5cc6d6df80d68b8955936d981601a739d24ffb3
parsalemi <parsalemi@outlook.com>
2026-07-26 08:14:26 +0330

    add: query for status and revoke session

--- src/db/queries/session.queries.ts
+++ src/db/queries/session.queries.ts
@@ -1,3 +1,4 @@
+import { Session } from '../../models/session.model.js';
 import knex from '../knex.js';
 
 function getActiveSessions(userId: string) {
@@ -5,6 +6,25 @@ function getActiveSessions(userId: string) {
   .where('expires_at', '>', knex.raw("NOW() - INTERVAL '30 minutes'"));
 }
 
+function isSessionExistsAndValid(refreshToken: string) {
+  return knex('sessions').where('refresh_token', refreshToken)
+  .where('is_rewoked', false).where('expires_at', '>', knex.client.raw('NOW()')).first();
+}
+
+function insertSession(session: Session) {
+  return knex('sessions').insert(session);
+}
+
+function revokeSession(refreshToken: string) {
+  return knex('sessions').where('refresh_token', refreshToken).update({
+    is_revoked: true,
+    updated_at: knex.fn.now()
+  })
+}
+
 export default {
-  getActiveSessions
+  getActiveSessions,
+  insertSession,
+  isSessionExistsAndValid,
+  revokeSession
 }
--- src/db/queries/user.queries.ts
+++ src/db/queries/user.queries.ts
@@ -1,19 +1,34 @@
+import { User } from '../../models/user.model.js';
 import knex from '../knex.js';
 
-function getUserById(id: string) {
+function getUserById(id: string): Promise<User> {
   return knex('users').where('id', id).first();
 }
 
-function getUserByEmail(email: string) {
+function getUserByEmail(email: string): Promise<User> {
   return knex('users').where('email', email).first();
 }
 
-function getUserByUsername(username: string) {
+function getUserByUsername(username: string): Promise<User> {
   return knex('users').where('username', username).first();
+}
+
+function registerUser(user: User) {
+  return knex('users').insert(user);
+}
+
+function updateStatus(user: User, status: 'online' | 'offline' | 'busy' | 'away') {
+  return knex('users').where('id', user.id).update({
+    status: status,
+    last_seen_at: knex.client.raw('NOW()'),
+    updated_at: knex.client.raw('NOW()'),
+  })
 }
 
 export default {
   getUserById,
   getUserByEmail,
-  getUserByUsername
+  getUserByUsername,
+  registerUser,
+  updateStatus
 }
